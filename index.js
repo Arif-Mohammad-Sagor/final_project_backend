86c21654335181327b5c26dd873ec2d0e92a8138
MdTalha707 <mdsagor0321@gmail.com>
2023-06-07 11:00:30 +0600

    testing

--- index.js
+++ index.js
@@ -1,9 +1,14 @@
 const express = require('express');
 const app = express();
 const port = process.env.PORT || 4000;
+const cors = require('cors')
+
+// middlewares
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
-   res.send(' hello world')
+   res.send(' hello World How are you ?')
 })
 app.listen(port, () => {
    console.log(` my port is running on port: ${port}`)
