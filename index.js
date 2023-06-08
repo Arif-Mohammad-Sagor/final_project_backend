0fb5e56073184882f62400e6b31e7b59ded09ace
MdTalha707 <mdsagor0321@gmail.com>
2023-06-08 22:32:53 +0600

    stripe payment intent created

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const app = express();
+const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
 require("dotenv").config();
 var jwt = require("jsonwebtoken");
 const port = process.env.PORT || 4000;
 const cors = require('cors')
 
+const stripe = require("stripe")(process.env.STRIPE_SECRET_KEY);
 
 // middlewares
 app.use(cors());
@@ -29,10 +31,8 @@ const verifyJWT = (req, res, next) => {
   })
 }
 
-const { MongoClient, ServerApiVersion, ObjectId } = require("mongodb");
-const uri =
-  `mongodb+srv://mdSagor:WpCXE6hxYmlQlcZz@cluster0.gmwr7s9.mongodb.net/?retryWrites=true&w=majority`;
 
+const uri = `mongodb://mdSagor:WpCXE6hxYmlQlcZz@ac-voaoh7g-shard-00-00.gmwr7s9.mongodb.net:27017,ac-voaoh7g-shard-00-01.gmwr7s9.mongodb.net:27017,ac-voaoh7g-shard-00-02.gmwr7s9.mongodb.net:27017/?ssl=true&replicaSet=atlas-xr8fvm-shard-0&authSource=admin&retryWrites=true&w=majority`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
   serverApi: {
@@ -50,6 +50,7 @@ async function run() {
     const instructorsCollection = client.db("Languages").collection("Instructors");
     const classesCollection = client.db("Languages").collection("Classes");
     const selectedClassesCollection = client.db("Languages").collection("selectedClasses");
+    const paymentsCollection = client.db("Languages").collection("payments");
 
     app.post("/jwt", (req, res) => {
       const userInfo = req.body;
@@ -68,6 +69,7 @@ async function run() {
       const result = await classesCollection.find().toArray();
       res.send(result);
     });
+    // selected classes apis here
     app.get("/selectedClasses",verifyJWT, async (req, res) => {
       const email = req.query.email;
       const query = { Email:email }
@@ -85,7 +87,57 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await selectedClassesCollection.deleteOne(query);
       res.send(result);
-   });
+    });
+// payment related apis here //
+// Todo :have to verify user here
+    app.post("/create-payment-intent", async (req, res) => {
+      const { price } = req.body;
+      const amount = price
+
+      // Create a PaymentIntent with the order amount and currency
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: parseInt(amount * 100),
+        currency: "INR",
+        description: "software compary",
+        shipping: {
+          name: "LalSign",
+          address: {
+            line1: "510 Townsend St",
+            postal_code: "98140",
+            city: "Sahinbag Delhi",
+            state: "New Delhi",
+            country: "India",
+          },
+        },
+        payment_method_types: ["card"],
+      });
+
+      res.send({
+        clientSecret: paymentIntent.client_secret,
+      });
+    });
+
+    // user payment apis here
+    app.post('/makepayment', async (req, res) => {
+      const payment = req.body;
+      // here converting into new object id ;
+          payment.haveInAllClassItemsId = payment.haveInAllClassItemsId.map(
+            (item) => new ObjectId(item)
+          );
+          const result = await paymentsCollection.insertOne(payment);
+    // here converting into new object Id selected id
+          const query = {
+            _id: {
+              $in: payment.selectedClassItemsId.map((id) => new ObjectId(id)),
+            },
+          };
+      const deletedSelectedClasses = await selectedClassesCollection.deleteMany(query);
+          
+          res.status(200).send({
+            result,
+            deletedSelectedClasses,
+          });
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
